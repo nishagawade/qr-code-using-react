ef281f844a39125d6d3575d0938818891e94f5f7
nisha.g <nisha.g@finacus.co.in>
2024-05-28 17:33:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
+
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState();
+	const [back, setBack] = useState('#FFFFFF');
+	const [fore, setFore] = useState('#000000');
+	const [size, setSize] = useState(256);
+
+	return (
+		<div className="App">
+			<center>
+				<br />
+				<br />
+				<input
+					type="text"
+					onChange={(e) => setValue(e.target.value)}
+					placeholder="Value of Qr-code"
+				/>
+				<br />
+				<br />
+				<input
+					type="text"
+					onChange={(e) => setBack(e.target.value)}
+					placeholder="Background color"
+				/>
+				<br />
+				<br />
+				<input
+					type="text"
+					onChange={(e) => setFore(e.target.value)}
+					placeholder="Foreground color"
+				/>
+				<br />
+				<br />
+				<input
+					type="number"
+					onChange={(e) => setSize(parseInt(e.target.value ===
+						'' ? 0 : e.target.value, 10))}
+					placeholder="Size of Qr-code"
+				/>
+				<br />
+				<br />
+				<br />
+				{value && (
+					<QRCode
+						title="GeeksForGeeks"
+						value={value}
+						bgColor={back}
+						fgColor={fore}
+						size={size === '' ? 0 : size}
+					/>
+				)}
+			</center>
+		</div>
+	);
 }
 
 export default App;
